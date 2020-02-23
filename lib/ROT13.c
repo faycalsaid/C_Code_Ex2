a9b0623c891b44739ffddbc20aecb809478be63a
feycce <faycalhaboo@gmail.com>
2020-02-23 23:16:58 +0100

    ROT13 Codded

--- lib/ROT13.c
+++ lib/ROT13.c
@@ -1,13 +1,26 @@
 #include "ROT13.h"
 
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 char* chiffre_ROT13(char* clair)
 {
-    return NULL;
+    int i=0;
+    while(clair[i] != '\0')
+    {
+        if(clair[i]>='A' && clair[i]<='Z')
+            clair[i] = (clair[i] - 'A' + 13) % 26 + 'A';
+        if(clair[i]>='a' && clair[i]<='z')
+            clair[i]= (clair[i] - 'a' + 13) % 26 + 'a';
+
+        i++;
+    }
+
+  return clair;
 }
 
 char* dechiffre_ROT13(char* chiffre)
 {
-    return NULL;
+    return chiffre_ROT13(chiffre);
 }
