b01779703839e632983e67295aca84ae882b548b
feycce <faycalhaboo@gmail.com>
2020-02-26 23:55:00 +0100

    BONUS1 fixed

--- lib/ROT13.c
+++ lib/ROT13.c
@@ -3,24 +3,14 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <Cesar.h>
 
 char* chiffre_ROT13(char* clair)
 {
-    int i=0;
-    while(clair[i] != '\0')
-    {
-        if(clair[i]>='A' && clair[i]<='Z')
-            clair[i] = (clair[i] - 'A' + 13) % 26 + 'A';
-        if(clair[i]>='a' && clair[i]<='z')
-            clair[i]= (clair[i] - 'a' + 13) % 26 + 'a';
-
-        i++;
-    }
-
-  return clair;
+    return chiffre_Cesar(clair, 'n');
 }
 
 char* dechiffre_ROT13(char* chiffre)
 {
-    return chiffre_ROT13(chiffre);
+    return dechiffre_Cesar(chiffre, 'n');
 }
